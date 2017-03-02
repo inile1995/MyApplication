e9bc85357a24035d250a63a49271c49b1ef12c8e
inile1995 <inile1995@gmail.com>
2017-03-02 09:43:22 +0700

    Show ListView ShortString

--- app/src/main/java/com/example/avh_home/myapplication/MainActivity.java
+++ app/src/main/java/com/example/avh_home/myapplication/MainActivity.java
@@ -8,14 +8,14 @@ import android.view.View;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    //Expisict
+    //Explicit
     ListView listView;
     private int[] ints = new int[]{R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
             R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08, R.drawable.traffic_09, R.drawable.traffic_10,
             R.drawable.traffic_11, R.drawable.traffic_12, R.drawable.traffic_13, R.drawable.traffic_14, R.drawable.traffic_15,
             R.drawable.traffic_16, R.drawable.traffic_17, R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
 
-    private String[] titleStrings,detailStrings;
+    private String[] titleStrings,detailStrings, shortStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +29,15 @@ public class MainActivity extends AppCompatActivity {
         titleStrings = getResources().getStringArray(R.array.title);
         detailStrings = getResources().getStringArray(R.array.detail);
 
+        //SubString detailString ตัดคำใน detailString เพื่อให้มีข้อความไม่เกิน 30 character
+        shortStrings = new String[detailStrings.length]; // จองพื้นที่ให้หน่วยความจำ shortString
+            for (int i = 0; i<detailStrings.length; i++ ) {
+                shortStrings[i] = detailStrings[i].substring(0,29)+"...";
+        }
+        // end for
+
         //Create ListView
-        MyAdapter myAdapter = new MyAdapter(MainActivity.this, ints, titleStrings, detailStrings );
+        MyAdapter myAdapter = new MyAdapter(MainActivity.this, ints, titleStrings, shortStrings);
         listView.setAdapter(myAdapter);
 
     } //Main Method onCreate
